5c43e415f7da11a74de748373712bb82522da729
Ömer Selvi <omerbugra97@hotmail.com>
2019-09-19 01:16:45 +0300

    parse event data on html page by page

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 
 CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
-url = "http://www.biletix.com/search/TURKIYE/tr#!subcat_sb:alternatif$MUSIC"
+url = "http://www.biletix.com/search/TURKIYE/tr#!subcat_sb:heavy_metal$MUSIC"
 categories = ["alternatif", "blues", "dans_elektronik", "dunya_muzik", "heavy_metal", "jazz", "klasik", "latin_tango",
               "newage", "party", "pop", "rap_hiphop", "rock", "turksanat_halkmuzik", "other"]
-date = []
+events = []
 concert_name = []
 
 
@@ -17,33 +17,43 @@ def get_website_data(url):
         driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
         driver.get(url)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
-        # todo parse data for first page
         parse_page(soup)
         pages = [page.text for page in soup.findAll("li", {"class": "spages"})]
-        if len(pages) > 2:
+        if len(pages) >= 2:
             pages = pages[:len(pages) // 2]
-            print(pages)
             for page in pages:
                 js = "javascript:gotoPage(" + page + ")"
                 driver.execute_script(js)
                 soup = BeautifulSoup(driver.page_source, 'html.parser')
-                print(soup)
                 parse_page(soup)
-                # todo parse data for each page
-        # implement pages here
-        # js = "javascript:gotoPage(25)"
-        # driver.execute_script(js)
-        # soup = BeautifulSoup(driver.page_source, 'html.parser')
         driver.quit()
-        # print(soup)
         return soup
     except:
         print("failed to connect")
 
 
 def parse_page(soup):
-    # todo implement this
+    day_of_week = [div.text for div in soup.select(".searchMobileDateDayName")][1:]
+    day_of_month = [div.text for div in soup.select(".searchMobileDateDayNumber")][1:]
+    month = [div.text for div in soup.select(".searchMobileDateMonth")][1:]
+    event_name = [div.text for div in soup.select(".searchResultEventName")][1:]
+    event_city = [div.text for div in soup.select(".searchResultCity")][1:]
+    event_place = [div.text for div in soup.select(".searchResultPlace")][1:]
+
+    if len(day_of_week) == len(day_of_month) and len(day_of_month) == len(month) and len(month) == len(event_city)\
+            and len(event_city) == len(event_place):
+        for i in range(0, len(day_of_week)):
+            events.append(day_of_month[i] + "/" + month[i] + "/" + day_of_week[i] + " - " + event_name[i] + " - " +
+                          event_city[i] + " - " + event_place[i])
+    else:
+        print("theres a bug, fix it")
+    # print(date)
+    # for i in range(0, len(day_of_week)):
+    #    print(da)
+    #print(data)
     return soup
 
 
 soup = get_website_data(url)
+for event in events:
+    print(event)
